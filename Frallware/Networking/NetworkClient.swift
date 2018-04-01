78b9331f55645c28f83b7e9dc587c629f6a4d9af
Fredrik Bystam <fredrik.bystam@gmail.com>
2018-04-01 10:46:34 +0200

    housekeeping

--- Frallware/Networking/NetworkClient.swift
+++ Frallware/Networking/NetworkClient.swift
@@ -88,3 +88,15 @@ public extension NetworkClient {
         return networkTask
     }
 }
+
+private struct MissingBodyError: Error {
+
+    let url: URL?
+
+    var localizedDescription: String {
+        guard let url = url else {
+            return "Response body was empty"
+        }
+        return "Response body was empty for URL: \(url)"
+    }
+}
